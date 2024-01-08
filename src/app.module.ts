6c480ed3bd44474c9633f742971f351c5bc38acf
motojate <motojate@naver.com>
2024-01-08 18:54:14 +0900

    * user 테이블을 삭제함에 따라, 관련 로직 모두 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './shared/prisma/prisma.module';
 import { RankModule } from './user/rank/rank.module';
-import { UserController } from './user/user.controller';
-import { UserModule } from './user/user.module';
-import { UserService } from './user/user.service';
 
 @Module({
-  imports: [PrismaModule, RankModule, UserModule],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService],
+  imports: [PrismaModule, RankModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
