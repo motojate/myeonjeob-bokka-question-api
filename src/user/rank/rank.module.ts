130489d16879ee47e393ffd035d0659c3581a11e
motojate <motojate@naver.com>
2024-01-08 19:01:38 +0900

    * 랭크 시스템 init 설정

--- src/user/rank/rank.module.ts
+++ src/user/rank/rank.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RankService } from './rank.service';
 import { RankController } from './rank.controller';
+import { PrismaService } from 'src/shared/prisma/prisma.service';
 
 @Module({
-  providers: [RankService],
+  providers: [RankService, PrismaService],
   controllers: [RankController],
 })
 export class RankModule {}
--- src/user/rank/rank.service.ts
+++ src/user/rank/rank.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { from, map } from 'rxjs';
+import { PrismaService } from 'src/shared/prisma/prisma.service';
 
 @Injectable()
-export class RankService {}
+export class RankService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  findRank(dto: { userSeq: string }) {
+    return from(
+      this.prisma.rank.findUnique({ where: { userSeq: dto.userSeq } }),
+    ).pipe(
+      map((data) => {
+        if (!data) this.initUserRank(dto);
+      }),
+    );
+  }
+  private initUserRank(dto: { userSeq: string }) {
+    return from(this.prisma.rank.create({ data: { userSeq: dto.userSeq } }));
+  }
+}
